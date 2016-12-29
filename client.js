1160a38e4cda27f14a1bd5ea4a1ad76c496b79fa
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-28 20:57:54 -1000

    chagnes

--- client.js
+++ client.js
@@ -3,3 +3,11 @@
 const net = require('net');
 
 let client = net.createConnection(8080, 'localhost');
+
+client.on("connect", (chunk) => {
+  console.log("connected...");
+});
+
+client.on("data", (chunk) => {
+  process.stdout.write(chunk);
+});
--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 
 const net = require('net');
 
-let server = net.createServer((socket) => {
+let server = net.createServer((request, response) => {
+
+  request.write(`Welcome to the server!\n`);
+  request.pipe(request);
+
+  request.on('data', )
+
+
 
 });
 
 server.listen(8080, '0.0.0.0', () => {
-  console.log('opened server on ', sever.address());
+  console.log('opened server on ', server.address());
 });
