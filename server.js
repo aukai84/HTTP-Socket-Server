3f5ccd4936b9dc246c5b6bc98098b6dff382bf7d
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-12-28 20:56:56 -1000

    made somethinh work

--- server.js
+++ server.js
@@ -2,10 +2,23 @@
 
 const net = require('net');
 
-let server = net.createServer((socket) => {
+// let server = net.createServer((socket) => {
 
+// });
+
+var server = net.createServer(function (socket) {
+  socket.on('error', function(err) {
+    console.error('SOCKET ERROR:', err);
+  });
+
+  socket.on('data', function(data){
+    process.stdout.write(data);
+  });
+
+  // socket.write('Echo server\r\n');
+  // socket.pipe(socket);
 });
 
 server.listen(8080, '0.0.0.0', () => {
-  console.log('opened server on ', sever.address());
+  console.log('opened server on ', server.address());
 });
